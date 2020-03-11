361294f5d44da8c7c78fb896af7a4a7ca3f01057
Dariel Vasquez <darksalamence10@gmail.com>
2020-03-10 19:38:55 -0600

    Commit Final

--- examen/routes/api/employee.js
+++ examen/routes/api/employee.js
@@ -16,13 +16,90 @@ function initEmployee(db) {
       POST      /makeolder               age
    */
 
-  router.get('/all', (req, res, next) => {
-    /*
-    empModel.xyz( (err, docs)=>{
-      return res.status(200).json(docs);
-    });
-    */
+  //http://localhost:3000/api/employee/all
+  router.get('/all', (req, res) => {
+   empModel.getEmployees((err, users)=>{
+    if(err){
+      console.log(err);
+      return res.status(500).json({"error":"error"});
+    }
+    return res.status(200).json(users);
+  });
   });// all
+
+  //http://localhost:3000/api/employee/byid/5e68260c06118f0ae8a309b4
+router.get('/byid/:id',(req, res)=>{
+  var id =  req.params.id ;
+  empModel.getEmployeesById(id, (err, doc)=>{
+    if(err){
+      console.log(err);
+      return res.status(500).json({"error":"error"});
+    }
+    return res.status(200).json(doc);
+  });
+});
+
+// http://localhost:3000/api/employee/bycompany/CONJURICA
+router.get('/bycompany/:company',(req, res)=>{
+  var company =  req.params.company ;
+  empModel.getEmployeesByCompany(company, (err, doc)=>{
+    if(err){
+      console.log(err);
+      return res.status(500).json({"error":"error"});
+    }
+    return res.status(200).json(doc);
+  });
+});
+
+// http://localhost:3000/api/employee/bytag/aliquip
+router.get('/bytag/:tag',(req, res)=>{
+  var tag =  req.params.tag ;
+  empModel.getEmployeesByTag(tag, (err, doc)=>{
+    if(err){
+      console.log(err);
+      return res.status(500).json({"error":"error"});
+    }
+    return res.status(200).json(doc);
+  });
+});
+
+// http://localhost:3000/api/employee/addtag/:id
+router.post('/addtag/:id', (req, res, next)=>{
+  var id = req.params.id;
+  var tag = req.body.tag;
+
+  empModel.addEmployeeATag(tag,id, (err, result)=>{
+    if(err){
+      console.log(err);
+      return res.status(500).json({"error":"error"});
+    }
+    return res.status(200).json(result);
+  });
+});
+
+//http://localhost:3000/api/employee/delete/5c871f24fcc06c827a5818c6
+router.delete('/delete/:id', (req, res)=>{
+  var id = req.params.id;
+  empModel.removeEmployee(id, (err, deletedDoc)=>{
+    if(err){
+      console.log(err);
+      return res.status(500).json({"error":"error"});
+    }
+    return res.status(200).json(deletedDoc);
+  });
+});
+
+//http://localhost:3000/api/employee/makeolder
+router.post('/makeolder', (req, res, next)=>{
+  var data = req.body;
+  empModel.increaseAgeToAll(data, (err, result)=>{
+    if(err){
+      console.log(err);
+      return res.status(500).json({"error":"error"});
+    }
+    return res.status(200).json(result);
+  });
+});
 
   
   return router;
--- examen/routes/api/employeeModel.js
+++ examen/routes/api/employeeModel.js
@@ -3,23 +3,46 @@ var ObjectID = require('mongodb').ObjectID;
 function employeeModel(db){
   var lib = {};
   var empColl = db.collection('employees');
+
   lib.getEmployees = (handler)=>{
     // implementar
     // obtener todos los documentos
-    return handler(new Error("No Implementado"), null);
+    empColl.find({}).toArray(handler);
   }
 
   lib.getEmployeesById = (id, handler) => {
     // implementar
     // Obtener un Documento solo mostrar
     // email, phone, name y age
-    return handler(new Error("No Implementado"), null);
+    var query = { "_id": new ObjectID(id) };
+    var projection = {"email":1, "phone":1, "name":1, "age":1, "_id": 0};
+    empColl.findOne(
+      query,
+      {"projection":projection},
+      (err, doc) => {
+        if (err) {
+          return handler(err, null);
+        }
+        return handler(null, doc);
+      }
+    );
   }
 
   lib.getEmployeesByCompany = (company, handler) => {
     // implementar
     // solo mostrar name, email, company
-    return handler(new Error("No Implementado"), null);
+    var query = { "company": company};
+    var projection = {"name":1, "email":1, "company":1, "_id":0};
+    empColl.findOne(
+      query,
+      {"projection":projection},
+      (err, doc) => {
+        if (err) {
+          return handler(err, null);
+        }
+        return handler(null, doc);
+      }
+    );
   }
 
   lib.getEmployeesByTag = (tag, handler) => {
@@ -28,19 +51,55 @@ function employeeModel(db){
     // al menos una vez el tag dentro del arreglo
     // tags
     // mostrar solo name, email, tags
-    return handler(new Error("No Implementado"), null);
+    var query = { "tags": tag };
+    var projection = {"name":1, "email":1, "tags":1, "_id":0};
+    empColl.findOne(
+      query,
+      {"projection":projection},
+      (err, doc) => {
+        if (err) {
+          return handler(err, null);
+        }
+        return handler(null, doc);
+      }
+    );
   }
 
   lib.addEmployeeATag = ( tag, id, handler) => {
     //Implementar
     //Se requiere agregar a un documento un nuevo tag
     // $push
+    var query = {"_id": new ObjectID(id)};
+    var postCommand = {
+      "$push":{
+        "tags": tag
+      }
+    };
+    empColl.updateOne(query, postCommand, (err, doc)=>{
+      if (err) {
+        console.log(err);
+        return handler(err, null);
+      }
+      return handler(null, doc);
+    });
     return handler(new Error("No Implementado"), null);
   }
 
   lib.removeEmployee = (id, handler) => {
     //Implementar
     //Se requiere eliminar un documento de la colección
+    lib.removeEmployee = (id, handler)=>{
+      var query = {"_id": new ObjectID(id)};
+      empColl.deleteOne(
+        query,
+        (err, rslt)=>{
+          if(err){
+            return handler(err, null);
+          }
+          return handler(null, rslt.result);
+        }
+      );
+    }
     return handler(new Error("No Implementado"), null);
   }
 
@@ -48,7 +107,19 @@ function employeeModel(db){
     //Implementar
     //Se requiere modificar todos los documentos de la colección
     // incrementando age por la cantidad de ageDelta $inc
-    return handler(new Error("No Implementado"), null);
+    var { ageToAdd } = ageDelta;
+    var updateCommand = {
+      "$inc":{
+        "age":parseInt(ageToAdd)
+      }
+    };
+  empColl.updateMany({},updateCommand, (err, updateResult)=>{
+    if (err) {
+      console.log(err);
+      return handler(err, null);
+    }
+    return handler(null, updateResult);
+  });
   }
   return lib;
 }
